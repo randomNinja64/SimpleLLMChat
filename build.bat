2c487cc3090fb57c006900f08ce43c88eabb1301
randomNinja64 <43556413+randomNinja64@users.noreply.github.com>
2026-03-23 23:29:39 -0500

    Reorganization and Build Script

---
+++ build.bat
@@ -0,0 +1,27 @@
+@echo off
+set ROOT=%~dp0
+set OUT=%ROOT%SimpleLLMChat
+
+:: Find MSBuild
+for /f "usebackq delims=" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do set MSBUILD=%%i
+if not defined MSBUILD (echo ERROR: MSBuild not found. & exit /b 1)
+
+:: Build solution
+echo Building solution ...
+"%MSBUILD%" "%ROOT%SimpleLLMChat.sln" /p:Configuration=Release /m /v:minimal || exit /b %ERRORLEVEL%
+echo.
+
+:: Stage output
+if exist "%OUT%" rmdir /s /q "%OUT%"
+mkdir "%OUT%"
+
+copy /Y "%ROOT%SimpleLLMChatCLI\bin\Release\SimpleLLMChatCLI.exe" "%OUT%\" >nul
+copy /Y "%ROOT%SimpleLLMChatGUI\bin\Release\SimpleLLMChatGUI.exe" "%OUT%\" >nul
+
+for %%T in (FileTools PythonTools ShellTools WebTools) do (
+    mkdir "%OUT%\tools\%%T"
+    copy /Y "%ROOT%Tools\%%T\bin\Release\%%T.exe"  "%OUT%\tools\%%T\" >nul
+    copy /Y "%ROOT%Tools\%%T\bin\Release\%%T.json" "%OUT%\tools\%%T\" >nul
+)
+
+echo Done: %OUT%
